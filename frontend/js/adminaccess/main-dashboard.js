61a2d3a843d69c92a35819278dde54f558eaa92b
Dennis Odro <dennishuago19@gmail.com>
2026-09-09 04:49:14 +0000

    fix stident admin access and add main dashboard js script

---
+++ frontend/js/adminaccess/main-dashboard.js
@@ -0,0 +1,8 @@
+'use strict'
+
+
+const studentButton = document.querySelector("#student-button");
+
+studentButton.addEventListener("click", function () {
+    window.location.href = "./student(admin-manage).html";
+});
